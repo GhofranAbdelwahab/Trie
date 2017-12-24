5d46b08f76fe2250b831688c36b18d5208f0ab98
Ghofran Abdelwahab <prog.ghofranabdelwahab@gmail.com>
2017-12-25 01:18:34 +0200

    Add files via upload

---
+++ Trie.java
@@ -0,0 +1,77 @@
+package acm;
+
+/**
+ *
+ * @author ghofranabdelwahab
+ */
+public class Trie {
+
+    TrieNode Root;
+
+    public Trie() {
+        Root = new TrieNode();
+    }
+
+    public void insertWord(String str) {
+        char[] charArr = str.toCharArray();
+        TrieNode start = Root;
+        for (int i = 0; i < charArr.length; i++) {
+            if (start.trie.containsKey(charArr[i])) {
+                start.trie.get(charArr[i]).incrementFrequencyPrefix();
+                if (i == charArr.length - 1) {
+                    start.trie.get(charArr[i]).setLeafTrue();
+                    start.trie.get(charArr[i]).incrementFrequencyWord();
+                } else {
+                    start = start.trie.get(charArr[i]);
+                }
+            } else {
+                start.trie.put(charArr[i], new TrieNode());
+                start.trie.get(charArr[i]).incrementFrequencyPrefix();
+                if (i == charArr.length - 1) {
+                    start.trie.get(charArr[i]).setLeafTrue();
+                    start.trie.get(charArr[i]).incrementFrequencyWord();
+
+                } else {
+                    start = start.trie.get(charArr[i]);
+                }
+
+            }
+        }
+    }
+
+    public int getFrequencyOfWord(String str) {
+        TrieNode start = Root;
+        char[] charArr = str.toCharArray();
+        for (int i = 0; i < charArr.length; i++) {
+            if (start.trie.containsKey(charArr[i])) {
+                if (i == charArr.length - 1 && start.trie.get(charArr[i]).isLeaf) {
+                    return start.trie.get(charArr[i]).frequencyWord;
+                } else {
+                    start = start.trie.get(charArr[i]);
+                }
+
+            } else {
+                return 0;
+            }
+        }
+        return 0;
+    }
+
+    public int getFrequencyOfPrefix(String str) {
+        TrieNode start = Root;
+        char[] charArr = str.toCharArray();
+        for (int i = 0; i < charArr.length; i++) {
+            if (start.trie.containsKey(charArr[i])) {
+                if (i == charArr.length - 1) {
+                    return start.trie.get(charArr[i]).frequencyPrefix;
+                } else {
+                    start = start.trie.get(charArr[i]);
+                }
+
+            } else {
+                return 0;
+            }
+        }
+        return 0;
+    }
+}
---
+++ TrieNode.java
@@ -0,0 +1,29 @@
+package acm;
+import java.util.HashMap;
+
+/**
+ *
+ * @author ghofranabdelwahab
+ */
+public class TrieNode {
+    HashMap<Character, TrieNode> trie;
+    boolean isLeaf;
+    int frequencyWord;
+    int frequencyPrefix;
+
+    public TrieNode() {
+        isLeaf = false;
+        frequencyWord = 0;
+        frequencyPrefix = 0;
+        trie = new HashMap<>();
+    }
+    public void setLeafTrue(){
+        this.isLeaf = true;
+    }
+    public void incrementFrequencyWord(){
+        this.frequencyWord++;
+    }
+    public void incrementFrequencyPrefix(){
+        this.frequencyPrefix++;
+    }
+}
